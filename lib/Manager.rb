16e866bbd268869fbe83ce1b533eea9fe91cc24d
rmiri <re_miriuk@hotmail.com>
2019-12-15 01:03:23 +0000

    the office management

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,33 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+    
+    @@managers_all = [] 
+    @@all_departments = []
+
+    def initialize(name:,department:,age:)
+        @name = name
+        @department = department
+        @age = age
+
+        @@managers_all << self
+
+        @@all_departments << department
+    end
+    def employees 
+        Employee.all.select{|emp_e| emp_e.manager == self}
+    end
+    def self.all
+        @@managers_all
+    end
+    def hire_employee(name,salary)
+        Employee.new(name:name,salary:salary,manager:self)
+    end
+    def self.all_departments
+        @@all_departments
+    end
+    def self.average_age
+        an_array = @@managers_all.map{|managers| managers.age}
+        an_array.reduce(:+) / @@managers_all.length
+    end
 end
--- run.rb
+++ run.rb
@@ -2,6 +2,17 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
+michael = Manager.new(name:"Michael Scott",department:"sales US",age:40)
+david = Manager.new(name:"David Brent",department:"sales UK",age:42)
+
+pam = Employee.new(name:"Pam Beesly",salary:15000,manager:michael)
+dwight = Employee.new(name:"Dwight Schrute",salary:15500,manager:michael)
+
+
+david.hire_employee("Jim Halpert",25000)
+
+
+
 
 #Test your code here
 
