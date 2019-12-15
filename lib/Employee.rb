85003f488e05c5bde9a45e25ca737714ee63a878
Sara Fotros <sarafotros@users.noreply.github.com>
2019-12-15 15:45:19 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,39 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        manager.name
+    end
+
+    def self.paid_over(pay)
+        self.all.select{|emp| emp.salary > pay}
+    end
+
+    def self.find_by_department(dept)
+        self.all.detect{|emp|emp.manager.department == dept} #detect finds the first one :)
+    end
+
+
+    def tax_bracket
+        Employee.all.select{|emplo|emplo.salary >1000}
+    end
+
+    
+
+
+
 end
